31c15c2dd8b3d7c8aeb0c8e02fbda5ca06c79809
Thirumeni2811 <thirumeni948@gmail.com>
2024-07-14 16:30:31 +0530

    Project component completed - error resolved

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { BrowserRouter , Routes , Route } from 'react-router-dom';
 import Portfolio from './Components/Portfolio';
-import Bank from './Components/Bank';
-import Ploughing from './Components/Ploughing';
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={< Portfolio/>} />
-          <Route path="/bank" element={< Bank/>} />
-          <Route path='/plough' element={<Ploughing />} />
         </Routes>
       </BrowserRouter>
     </>
