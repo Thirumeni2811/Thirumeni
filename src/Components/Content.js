72e7872efc55bce59c98a47303ae7152b847cf56
Thirumeni2811 <thirumeni948@gmail.com>
2024-09-15 22:04:30 +0530

    Resume updated

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -125,7 +125,7 @@ const News = ({ data }) => {
                 Focused and MERN stack developer with a keen interest in software development and artificial intelligence. By comprehensive exposure to the underlying concepts and applying them vividly to few projects in front end, my love for these domains came into being.
               </p>
               <div className='buttonLeft'>
-                <a href='https://drive.google.com/file/d/1YsEWNHwsBZcXFfHQqfIavAyFNbk_RB57/view?usp=sharing' target='_blank' rel='noopener noreferrer'>               
+                <a href='https://drive.google.com/file/d/1XpBKugqs7VW852-llaCKrZ3awpm3CzlU/view?usp=sharing' target='_blank' rel='noopener noreferrer'>               
                    <Button className='color-primary'>Download Resume</Button>
                 </a>
               </div>
